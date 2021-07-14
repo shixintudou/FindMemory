71ac5810a71f475db099aab00b5b5b7a53c9243f
shixintudou <2661554122@qq.com>
2021-07-14 16:05:54 +0800

    Scripts

---
+++ Assets/Scripts/BaseActor.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseActor: MonoBehaviour
+{
+    public enum State
+    {
+        Idle,Run,Jump,Attack,Behurt,Dead
+    }
+
+    public State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = State.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                StateIdle();
+                break;
+            case State.Jump:
+                StateJump();
+                break;
+            case State.Run:
+                StateRun();
+                break;
+            case State.Attack:
+                StateAttack();
+                break;
+            case State.Behurt:
+                StateBehurt();
+                break;
+            case State.Dead:
+                StateDead();
+                break;
+        }
+    }
+    public virtual void StateIdle()
+    {
+
+    }
+    public virtual void StateJump()
+    {
+
+    }
+    public virtual void StateRun()
+    {
+
+    }
+    public virtual void StateAttack()
+    {
+
+    }
+    public virtual void StateBehurt()
+    {
+
+    }
+    public virtual void StateDead()
+    {
+
+    }
+
+
+
+    public virtual void ChangeToIdle()
+    {
+        state = State.Idle;
+    }
+    public virtual void ChangeToRun()
+    {
+        state = State.Run;
+    }
+    public virtual void ChangeToJump()
+    {
+        state = State.Jump;
+    }
+    public virtual void ChangeToAttack()
+    {
+        state = State.Attack;
+    }
+    public virtual void ChangeToBehurt()
+    {
+        state = State.Behurt;
+    }
+    public virtual void ChangeToDead()
+    {
+        state = State.Dead;
+    }
+}
---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : BaseActor
+{
+    public float speed;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void StateRun()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        if(x>0)
+        {
+            
+        }
+    }
+}
